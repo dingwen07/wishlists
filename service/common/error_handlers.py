cc67c2571f0c160c1aea43114048bf0b130a027d
Dingwen Wang <wangdingwen32@gmail.com>
2025-11-29 09:21:25 -0500

    Update error_handlers.py

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -16,7 +16,6 @@
 """
 Module: error_handlers
 """
-from flask import jsonify
 from flask import current_app as app  # Import Flask application
 from service.models import DataValidationError
 from . import status
